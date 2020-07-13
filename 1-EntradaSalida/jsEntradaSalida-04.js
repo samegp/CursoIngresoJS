8f026e851501e3763111c937260512af8ec87e1b
Samuel <samuelguzmanp@gmail.com>
2020-07-13 12:32:09 -0300

    primera clase de programacion

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,16 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	
+	let nombre;
+
+	nombre = prompt('ingrese su nombre');
+
+	document.getElementById('txtIdNombre').value = nombre;
+
+
+
+
+
 }
 
